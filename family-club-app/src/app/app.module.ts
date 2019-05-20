2dd368bc0f3db6a69a7651a0557c8cb3da121d64
DZ in The House <icelffs@gmail.com>
2019-05-21 00:13:58 +0800

    add accounting component

--- family-club-app/src/app/app.module.ts
+++ family-club-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
+import { AccountingComponent } from './accounting/accounting.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SideBarComponent
+    SideBarComponent,
+    AccountingComponent
   ],
   imports: [
     BrowserModule,
