9a4aa5f7b1b74d4efb9879c68ca2a4de86c753d9
DZ in The House <icelffs@gmail.com>
2019-05-21 00:14:14 +0800

    add accounting routing

--- family-club-app/src/app/app-routing.module.ts
+++ family-club-app/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccountingComponent } from './accounting/accounting.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ {path: 'accounting', component: AccountingComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
