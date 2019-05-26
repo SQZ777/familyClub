8a3fad11d5bc09223bc3bc651e6d12c04bf27061
DZ in The House <icelffs@gmail.com>
2019-05-26 21:03:18 +0800

    add http module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AccountingComponent } from './accounting/accounting.component';
+import { HttpClientModule } from '@angular/common/http';
 
 const routes: Routes = [
- {path: 'accounting', component: AccountingComponent}
+  { path: 'accounting', component: AccountingComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    HttpClientModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
