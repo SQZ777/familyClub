b471ef34e1743505b4071f18c35b146c9de8162d
DZ in The House <icelffs@gmail.com>
2019-05-26 21:03:35 +0800

    add accounting record class

---
+++ src/app/accounting/accountingRecord.ts
@@ -0,0 +1,10 @@
+export class AccountingRecord {
+    id: number;
+    userName: string;
+    costType: string;
+    item: string;
+    account: string;
+    cost: number;
+    date: number;
+    currency: string;
+}
