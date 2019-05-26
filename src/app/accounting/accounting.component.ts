835896e5a283f29d3a693443a2482b466cf54840
DZ in The House <icelffs@gmail.com>
2019-05-26 21:03:55 +0800

    add table and mock data

--- src/app/accounting/accounting.component.ts
+++ src/app/accounting/accounting.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountingRecord } from './accountingRecord';
 
 @Component({
   selector: 'app-accounting',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./accounting.component.scss']
 })
 export class AccountingComponent implements OnInit {
+  httpClient: any;
 
   constructor() { }
+  accountingRecords: AccountingRecord[] = [
+    { id: 1, userName: '煞氣欸哥', costType: '食物', item: '測試用食物', account: 'TWD', cost: 10, date: Date.now(), currency: '' },
+    { id: 1, userName: '煞氣欸哥', costType: '食物', item: '測試用食物', account: 'TWD', cost: 10, date: Date.now(), currency: '' },
+    { id: 1, userName: '煞氣欸哥', costType: '食物', item: '測試用食物', account: 'TWD', cost: 10, date: Date.now(), currency: '' },
+    { id: 1, userName: '煞氣欸哥', costType: '食物', item: '測試用食物', account: 'TWD', cost: 10, date: Date.now(), currency: '' }
+  ];
 
   ngOnInit() {
+  }
+
+  getAccountingRecords() {
+    const url = 'https://b3d930df-dd58-4968-8f4e-ebecd968a934.mock.pstmn.io/accountingRecords/get';
   }
 
 }
